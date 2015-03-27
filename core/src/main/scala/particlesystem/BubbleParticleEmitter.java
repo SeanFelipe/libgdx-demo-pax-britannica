150d46367860ebabd65955ce7b1d47f002381f65
Sean Wolfe <ether.joe@gmail.com>
2015-03-26 22:39:26 -0700

    move particle system files to scala folder

---
+++ core/src/main/scala/particlesystem/BubbleParticleEmitter.java
@@ -0,0 +1,25 @@
+package srg.scala.paxbritannica.particlesystem;
+
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+
+import srg.scala.paxbritannica.Resources;
+
+public class BubbleParticleEmitter extends ParticleEmitter {
+
+	public BubbleParticleEmitter() {
+		super();
+				
+		life = 2.5f;
+		damping =1f;
+		
+		set(Resources.getInstance().bubble);
+	}
+
+	public void addParticle(Vector2 position) {
+		addParticle(position, random.set(MathUtils.random() * 0.1f - 0.05f, 0.01f + MathUtils.random() * 0.05f) ,life, 1);
+	}
+	
+	
+
+}
